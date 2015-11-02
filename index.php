2a2cab3a9926b77a51ee056041d7e06dc97a1f65
maywanting <maywanting@gmail.com>
2015-11-02 22:31:53 +0800

    init commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+function dd ($arg) {
+	echo "<pre>";
+	var_dump($arg);
+	echo "</pre>";
+	die();
+}
+
+function d ($arg) {
+	echo "<pre>";
+	var_dump($arg);
+	echo "</pre>";
+}
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
